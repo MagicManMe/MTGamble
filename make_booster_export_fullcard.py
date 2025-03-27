0c682aea2a5b2f073630a207f25ab18a7ac9a721
jake <magicmanme@live.com>
2025-03-27 13:03:57 -0400

    Program now uses STRICTLY SET CODES. Still case insensitive There is a list of the sets and associated codes if the name is needed somewhere.

--- make_booster_export_fullcard.py
+++ make_booster_export_fullcard.py
@@ -52,7 +52,6 @@ def lowercase_keys(input_dict):
 sets = get_set_dict()
 
 #create a lowercase set dictionary for Clayton booster function
-lower_sets = lowercase_keys(sets)
 
 
 #Get all cards in a set and return a list of the card objects
@@ -132,6 +131,8 @@ def get_foil_wildcards_in_set(set_code: str) -> list:
 
 #makes a booster pack and returns a list of the card objects
 def make_play_booster(set_code: str):
+    # Makes set_code case-insensitive
+    set_code = set_code.casefold()
     booster = []
 
     #get commons
@@ -166,13 +167,15 @@ def make_play_booster(set_code: str):
     return booster
 
 #Creates a booster pack and saves it to a json file
-def export_play_booster(set_name: str):
+def export_play_booster(set_code: str):
+    # Makes set_code case-insensitive
+    set_code = set_code.casefold()
     #Makes the booster pack
-    booster = make_play_booster(lower_sets[set_name.casefold()])
+    booster = make_play_booster(set_code)
 
     #Creates a unique filename, so you don't overwrite any boosters you've made
     time_now = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
-    JSON_NAME = f"boosters/{set_name.casefold()} Booster {time_now}.json"
+    JSON_NAME = f"boosters/{set_code} Booster {time_now}.json"
 
     #Saves the booster pack to a json file
     with open(JSON_NAME, 'w', encoding='utf-8') as file:
@@ -182,28 +185,31 @@ def export_play_booster(set_name: str):
     print(f'Exported Booster Pack to {JSON_NAME}')
 
 #Example of exporting booster
-#export_play_booster('Aetherdrift')
+#export_play_booster('dft')
 
 
 #The Clayton booster function, uses lower_sets and casefold to be case insensitive
-def make_clayton_booster(set_name: str, booster_type: str) -> list[dict] | None:
-    #This checks if the provided set_name is a valid set, casefold() makes everything lowercase so the input can be case insensitive
-    if set_name.casefold() not in lower_sets.keys():
+def make_clayton_booster(set_code: str, booster_type: str) -> list[dict] | None:
+    #Makes set_code and booster_type case-insensitive
+    set_code = set_code.casefold()
+    booster_type = booster_type.casefold()
+    # This checks if the provided set_code is a valid set
+    if set_code not in sets.values():
         #Raise an Exception if the set_name is invalid
-        raise Exception(f"Invalid set name: {set_name.casefold()}")
+        raise Exception(f"Invalid set set code: {set_code}")
     else:
-        match booster_type.casefold():
+        match booster_type:
             case 'play':
-                #Call the make booster function, pass it lower_sets[set_name.casefold()] which gets the setcode from the setname while being case insensitive
-                return make_play_booster(lower_sets[set_name.casefold()])
+                #Call the make play booster function, case insensitive
+                return make_play_booster(set_code)
 
         #Raise an exception if the booster_type is invalid
-        raise Exception(f"Invalid booster type: {booster_type.casefold()}")
+        raise Exception(f"Invalid booster type: {booster_type}")
 
 
 
 #Examples of input checking
-print(make_clayton_booster('aetherdrift','play'))
-print(make_clayton_booster('aeTHerdrIft','play'))
-print(make_clayton_booster('aeTH erdrIft','play'))
-print(make_clayton_booster('aeTHerdrIft','commander'))
+#print(make_clayton_booster('dft','play'))
+#print(make_clayton_booster('DFT','play'))
+#print(make_clayton_booster('D F T','play'))
+#print(make_clayton_booster('dft','commander'))
